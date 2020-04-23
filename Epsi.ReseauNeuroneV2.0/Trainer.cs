458ca8120ca471a30144f179eeabb7b3b8ecb7ee
steve-evian74 <steve.bailleul74@gmail.Com>
2020-04-23 11:55:54 +0200

    Premier Commit

---
+++ Epsi.ReseauNeuroneV2.0/Trainer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Epsi.ReseauNeuroneV2._0
+{
+    class Trainer
+    {
+
+
+        public void Verification()
+        {
+
+        }
+
+
+        public void gradient()
+        {
+
+        }
+
+
+
+    }
+}
